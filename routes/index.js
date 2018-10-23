ffa8c4772f4064415049ba40b7201a85ed1bd32f
Victor Santana <victor@hepoca.com>
2018-10-23 17:16:26 -0500

    Add Request details at Index page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var hostname = os.hostname();
+  var ip = req.ip;
+  var ipfw = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  var useragent = req.headers['user-agent']
+
+  var htmlvar = {
+    hostname: hostname,
+    ip: ip,
+    ipfw: ipfw,
+    useragent: useragent,
+    title: 'Express'
+  };  
+  res.render('index', htmlvar);
 });
 
 module.exports = router;
