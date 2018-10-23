10a4d6279f41d05b9c17bbdf46fa2d042307e18a
Victor Santana <victor@hepoca.com>
2018-10-23 14:11:16 -0500

    Change snatportexhaustion to lowercase

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use('/css', express.static(__dirname + '/public/Content')); // redirect CSS 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
-app.use('/SNatPortExhaustion', snatportexhaustionRouter);
+app.use('/snatportexhaustion', snatportexhaustionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/snatportexhaustion.js
+++ routes/snatportexhaustion.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('SNatPortExhaustion', { title: 'Express' });
+  res.render('snatportexhaustion', { title: 'Express' });
 });
 
 module.exports = router;
