e38b66e6f77c8c3f63803aa7e6f25f409229d66a
welasco <26201510+Welasco@users.noreply.github.com>
2023-05-15 22:22:12 +0000

    added headers and environment

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var os = require('os')
+//let stringify = require('json-stringify-safe');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,14 +9,28 @@ router.get('/', function(req, res, next) {
   var ip = req.ip;
   var ipfw = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var useragent = req.headers['user-agent']
-
+  var environment = process.env.environment || 'Not Set'
+  // var rawheaders = req.rawHeaders.toString().split(',');
+  // var printrawheaders = "";
+  // for (let i = 0; i < rawheaders.length; i++) {
+  //   //const element = rawheaders[i];
+  //   //console.log(rawheaders[i]);
+  //   printrawheaders += rawheaders[i] + ": " + rawheaders[i+1] + "<br>";
+  //   i++
+  // }
+  //console.log(printrawheaders);
   var htmlvar = {
     hostname: hostname,
     ip: ip,
     ipfw: ipfw,
     useragent: useragent,
-    title: 'Express'
-  };  
+    title: 'Express',
+    environment: environment,
+    rawheaders: req.rawHeaders,
+    headers: JSON.stringify(req.headers)
+  };
+  //console.log(stringify(req.rawHeaders));
+  //console.log(req.rawHeaders);
   res.render('index', htmlvar);
 });
 
