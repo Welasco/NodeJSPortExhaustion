7dc9d9ee8909896f79465bc44a507effe3dad381
welasco <26201510+Welasco@users.noreply.github.com>
2023-05-15 22:25:33 +0000

    added headers and environment

--- routes/index.js
+++ routes/index.js
@@ -10,15 +10,6 @@ router.get('/', function(req, res, next) {
   var ipfw = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   var useragent = req.headers['user-agent']
   var environment = process.env.environment || 'Not Set'
-  // var rawheaders = req.rawHeaders.toString().split(',');
-  // var printrawheaders = "";
-  // for (let i = 0; i < rawheaders.length; i++) {
-  //   //const element = rawheaders[i];
-  //   //console.log(rawheaders[i]);
-  //   printrawheaders += rawheaders[i] + ": " + rawheaders[i+1] + "<br>";
-  //   i++
-  // }
-  //console.log(printrawheaders);
   var htmlvar = {
     hostname: hostname,
     ip: ip,
@@ -29,8 +20,6 @@ router.get('/', function(req, res, next) {
     rawheaders: req.rawHeaders,
     headers: JSON.stringify(req.headers)
   };
-  //console.log(stringify(req.rawHeaders));
-  //console.log(req.rawHeaders);
   res.render('index', htmlvar);
 });
 
